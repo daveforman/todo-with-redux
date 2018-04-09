dc32f9e508caae37853d785916e292dce40565b4
Dave <dave@stackden.com>
2018-04-09 13:08:50 -0700

    Middleware setup

---
+++ src/middleware/promise.js
@@ -0,0 +1,15 @@
+export default store => next => action => {
+    if(!action.payload || !action.payload.then){
+        console.log('No promisises here');
+        return next(action);
+    }
+
+    console.log('Found a promise');
+
+    action.payload.then(resp => {
+        const newAction = {...action, payload: resp};
+        store.dispatch(newAction);
+    });
+
+    return action.payload;
+}
